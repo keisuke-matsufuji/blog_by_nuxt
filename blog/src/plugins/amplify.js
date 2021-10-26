f22ad5e2a2da9c2f5ca684c56b1be7e62faffd17
Keisuke Matsufuji <mt.fuji06781@gmail.com>
2021-10-26 23:55:06 +0900

    コメントアウト

--- blog/src/plugins/amplify.js
+++ blog/src/plugins/amplify.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import Amplify from 'aws-amplify'
-import '@aws-amplify/ui-vue'
-import awsExports from '../../amplify/aws-exports'
+// import Vue from 'vue'
+// import Amplify from 'aws-amplify'
+// import '@aws-amplify/ui-vue'
+// import awsExports from '../../amplify/aws-exports'
 
-Amplify.configure(awsExports)
-Vue.use(Amplify)
+// Amplify.configure(awsExports)
+// Vue.use(Amplify)
